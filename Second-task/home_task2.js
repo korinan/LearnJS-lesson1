67f78380a76fec170e647a6fb3c862d04777da5c
Anastasiia_Korin <akoren65787@gmail.com>
2022-08-08 15:01:12 +0200

    Created third task

--- Second-task/home_task2.js
+++ Second-task/home_task2.js
@@ -1,5 +1,6 @@
 //Home task1
 
+
 const num = prompt("Введіть номер квитка");
 
     if ((+num[0]+ +num[1] + +num[2]) == (+num[3]+ +num[4] + +num[5])) {
@@ -35,3 +36,19 @@ else {
 }
 
 //Home task 3
+
+let length = prompt("Введіть довжину приміщення");
+let width = prompt("Введіть ширину приміщення");
+let height = prompt("Введіть висоту приміщення");
+
+const volumeRoom = length * width * height;
+const paint = volumeRoom / 16;
+
+    if (length >= 1000 || width >= 1000 || height >= 1000) {
+    alert("Занадто велике приміщення");
+
+} 
+    
+    else {
+    alert(`Вам потрібно ${paint} літрів фарби"`);
+}
