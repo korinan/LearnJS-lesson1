6628adb56cc5c2de298ae08dd1308523aa0e86ae
Anastasiia_Korin <akoren65787@gmail.com>
2022-08-10 23:21:59 +0200

    Solution task first

---
+++ Third-task/home_task3.js
@@ -0,0 +1,16 @@
+//Home task1 
+
+let strLength = prompt('Вкажіть довжину вашого рядка');
+let symbol = prompt('Вкажіть ваш символ');
+let line = '';
+for (i=0; i<strLength; i+=1){
+    line +=symbol;
+}
+console.log(line); 
+
+
+//Home task2
+
+//Home task3
+
+//Home task4
